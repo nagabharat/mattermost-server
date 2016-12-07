1e1b43bb735c0fdd464feef34dcf556649c8a392
Erik Gui <egui@uber.com>
2016-12-07 13:14:37 -0800

    UCHAT-151 modify the image attachment preview to match design

--- webapp/components/file_attachment.jsx
+++ webapp/components/file_attachment.jsx
@@ -11,6 +11,8 @@ import * as Utils from 'utils/utils.jsx';
 
 import {getFileUrl, getFileThumbnailUrl} from 'mattermost-redux/utils/file_utils';
 
+import downloadIcon from 'images/download_CTA.svg';
+
 export default class FileAttachment extends React.Component {
     constructor(props) {
         super(props);
@@ -66,32 +68,13 @@ export default class FileAttachment extends React.Component {
     render() {
         const fileInfo = this.props.fileInfo;
         const fileName = fileInfo.name;
+
         const fileUrl = getFileUrl(fileInfo.id);
+        const type = Utils.getFileType(fileInfo.extension);
 
         let thumbnail;
         if (this.state.loaded) {
-            const type = Utils.getFileType(fileInfo.extension);
-
-            if (type === 'image') {
-                let className = 'post-image';
-
-                if (fileInfo.width < Constants.THUMBNAIL_WIDTH && fileInfo.height < Constants.THUMBNAIL_HEIGHT) {
-                    className += ' small';
-                } else {
-                    className += ' normal';
-                }
-
-                thumbnail = (
-                    <div
-                        className={className}
-                        style={{
-                            backgroundImage: `url(${getFileThumbnailUrl(fileInfo.id)})`
-                        }}
-                    />
-                );
-            } else {
-                thumbnail = <div className={'file-icon ' + Utils.getIconClassName(type)}/>;
-            }
+            thumbnail = <div className={'file-icon ' + Utils.getIconClassName(type)}/>;
         } else {
             thumbnail = <div className='post-image__load'/>;
         }
@@ -186,23 +169,58 @@ export default class FileAttachment extends React.Component {
             );
         }
 
-        return (
-            <div className='post-image__column'>
-                <a
-                    className='post-image__thumbnail'
-                    href='#'
-                    onClick={this.onAttachmentClick}
-                >
-                    {thumbnail}
-                </a>
-                <div className='post-image__details'>
-                    {filenameOverlay}
-                    <div>
-                        {downloadButton}
-                        <span className='post-image__type'>{fileInfo.extension.toUpperCase()}</span>
-                        <span className='post-image__size'>{Utils.fileSizeToString(fileInfo.size)}</span>
+        let fileAttachment;
+        if (type === 'image' && !this.props.compactDisplay) {
+            fileAttachment = (
+                <div className='post-image__preview'>
+                    <a
+                        href='#'
+                        onClick={this.onAttachmentClick}
+                    >
+                        <img
+                            className='post-image__image'
+                            src={getFileThumbnailUrl(fileInfo.id)}
+                        />
+                    </a>
+                    <div className='post-image__info'>
+                        <a
+                            href={fileUrl}
+                            download={fileName}
+                            className='post-image__download'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                        >
+                            <img src={downloadIcon}/>
+                            {filenameOverlay}
+                            <span className='post-image__size'>{Utils.fileSizeToString(fileInfo.size)}</span>
+                        </a>
                     </div>
                 </div>
+            );
+        } else {
+            fileAttachment = (
+                <div className='post-image__column'>
+                    <a
+                        className='post-image__thumbnail'
+                        href='#'
+                        onClick={this.onAttachmentClick}
+                    >
+                        {thumbnail}
+                    </a>
+                    <div className='post-image__details'>
+                        {filenameOverlay}
+                        <div>
+                            {downloadButton}
+                            <span className='post-image__type'>{fileInfo.extension.toUpperCase()}</span>
+                            <span className='post-image__size'>{Utils.fileSizeToString(fileInfo.size)}</span>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        return (
+            <div>
+                {fileAttachment}
             </div>
         );
     }

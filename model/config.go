3abf13a3428f2024351314c028937c209b8510b7
George Goldberg <george@gberg.me>
2017-10-17 09:32:43 +0100

    PLT-7862: Support batched live indexing.

--- model/config.go
+++ model/config.go
@@ -139,6 +139,7 @@ const (
 	ELASTICSEARCH_SETTINGS_DEFAULT_AGGREGATE_POSTS_AFTER_DAYS      = 365
 	ELASTICSEARCH_SETTINGS_DEFAULT_POSTS_AGGREGATOR_JOB_START_TIME = "03:00"
 	ELASTICSEARCH_SETTINGS_DEFAULT_INDEX_PREFIX                    = ""
+	ELASTICSEARCH_SETTINGS_DEFAULT_LIVE_INDEXING_BATCH_SIZE        = 1
 )
 
 type ServiceSettings struct {
@@ -484,6 +485,7 @@ type ElasticsearchSettings struct {
 	AggregatePostsAfterDays     *int
 	PostsAggregatorJobStartTime *string
 	IndexPrefix                 *string
+	LiveIndexingBatchSize       *int
 }
 
 type DataRetentionSettings struct {
@@ -1592,6 +1594,16 @@ func (o *Config) SetDefaults() {
 		*o.DataRetentionSettings.Enable = false
 	}
 
+	if o.ElasticsearchSettings.IndexPrefix == nil {
+		o.ElasticsearchSettings.IndexPrefix = new(string)
+		*o.ElasticsearchSettings.IndexPrefix = ELASTICSEARCH_SETTINGS_DEFAULT_INDEX_PREFIX
+	}
+
+	if o.ElasticsearchSettings.LiveIndexingBatchSize == nil {
+		o.ElasticsearchSettings.LiveIndexingBatchSize = new(int)
+		*o.ElasticsearchSettings.LiveIndexingBatchSize = ELASTICSEARCH_SETTINGS_DEFAULT_LIVE_INDEXING_BATCH_SIZE
+	}
+
 	if o.JobSettings.RunJobs == nil {
 		o.JobSettings.RunJobs = new(bool)
 		*o.JobSettings.RunJobs = true
@@ -1846,6 +1858,10 @@ func (o *Config) IsValid() *AppError {
 
 	if _, err := time.Parse("03:04", *o.ElasticsearchSettings.PostsAggregatorJobStartTime); err != nil {
 		return NewAppError("Config.IsValid", "model.config.is_valid.elastic_search.posts_aggregator_job_start_time.app_error", nil, err.Error(), http.StatusBadRequest)
+	}
+
+	if *o.ElasticsearchSettings.LiveIndexingBatchSize < 1 {
+		return NewAppError("Config.IsValid", "model.config.is_valid.elastic_search.live_indexing_batch_size.app_error", nil, "", http.StatusBadRequest)
 	}
 
 	return nil

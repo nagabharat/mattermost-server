91c5b471a243c13e91ecb4f1c99618b06696f77d
Miguel Espinoza <espinozamiguel349@gmail.com>
2018-01-18 11:41:06 -0500

    Set Access-Control-Allow-Credentials to true in user login

--- api4/user.go
+++ api4/user.go
@@ -910,6 +910,7 @@ func login(c *Context, w http.ResponseWriter, r *http.Request) {
 
 	user.Sanitize(map[string]bool{})
 	w.Header().Set("Access-Control-Expose-Headers", "Token")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	w.Write([]byte(user.ToJson()))
 }
 
